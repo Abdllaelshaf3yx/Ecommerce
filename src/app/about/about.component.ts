55d046c729de755ea7f4b76350a550797ea80e74
abdllaelshaf3y <abdllaelshaf3y@gmail.com>
2024-10-23 03:14:38 +0300

    Update About

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
 })
 export class AboutComponent {}
